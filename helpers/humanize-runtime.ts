02891933b51ac78e11c887b211a03f36f369f087
Kinoo <148229042+getkino@users.noreply.github.com>
2025-07-31 10:33:42 +0300

    fix: handle runtimes longer than three hours

--- helpers/humanize-runtime.ts
+++ helpers/humanize-runtime.ts
@@ -1,15 +1,10 @@
 const humanizeRuntime = (amount: number) => {
   if (!amount) return "Unknown";
-  if (amount < 60) {
-    return `0h ${amount}m`;
-  }
-  if (amount < 120) {
-    return `1h ${amount - 60}m`;
-  }
-  if (amount < 180) {
-    return `2h ${amount - 120}m`;
-  }
-  return `3h ${amount - 180}m`;
+
+  const hours = Math.floor(amount / 60);
+  const minutes = amount % 60;
+
+  return `${hours}h ${minutes}m`;
 };
 
 export default humanizeRuntime;
